ce552878dc38f81db837d26bbea3a5045396606a
Tanmay Tiwari <tanmaytiwari0508@gmail.com>
2024-04-19 15:55:16 +0530

    issue of not using strategy pattern

---
+++ src/StrategyDesignPattern/IssueOfNotUsingStrategy.java
@@ -0,0 +1,32 @@
+package StrategyDesignPattern;
+
+public class IssueOfNotUsingStrategy {
+
+//    public interface Comp{
+//        public String company_des();
+//    }
+//
+//    public class A implements Comp{
+//        @Override
+//        public String company_des() {
+//            return "this is a product of apple company";
+//        }
+//    }
+//
+//    public class B implements Comp{
+//        @Override
+//        public String company_des() {
+//            return "this is a product of apple company";
+//        }
+//    }
+//
+//    public class C implements Comp{
+//        @Override
+//        public String company_des() {
+//            return "this is a product of Samsung company";
+//        }
+//    }
+
+    // here we can observe that the company_desc (i.e this is a product of apple company) is repeated at two places
+    // this is a case of code duplication, and we can resolve this issue using strategy pattern
+}
